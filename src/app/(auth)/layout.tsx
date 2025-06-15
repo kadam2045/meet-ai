118f4bd4fb1a96a90b26ec2048d958ebd71ee14b
Shubham Kadam <shubham.kadam@appgambit.com>
2025-06-15 09:51:48 +0530

    sidebar ui

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className=" bg-muted flex min-h-svh  flex-col items-center justify-center p-6 md:p-10">
+    <div className=" bg-muted flex min-h-svh w-full  flex-col items-center justify-center p-6 md:p-10">
       <div>{children}</div>
     </div>
   );
