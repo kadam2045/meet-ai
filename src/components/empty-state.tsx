46ea157867888e48e52380a8aae66d0b48cb3685
Shubham Kadam <shubham.kadam@appgambit.com>
2025-06-30 18:03:56 +0530

    21:Meeting Variants

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -4,15 +4,20 @@ import Image from "next/image";
 interface Props {
   title: string;
   description: string;
+  image?: string;
 }
 
-export const EmptyState = ({ title, description }: Props) => {
+export const EmptyState = ({
+  title,
+  description,
+  image = "/empty.svg",
+}: Props) => {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
-      <Image src="/empty.svg" alt="logo" width={240} height={240} />
+      <Image src={image} alt="logo" width={240} height={240} />
       <div className="flex flex-col gap-y-6 max-w-md mx-auto text-center">
         <h6 className="text-lg font-medium">{title}</h6>
-        <p className="text-sm text-muted-foreground">{description}</p>
+        <p className="text-md text-muted-foreground">{description}</p>
       </div>
     </div>
   );
